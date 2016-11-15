e95a9a190d557c7f27812f31b175453d17a2007b
jf248 <jf248@user.no-reply.github.com>
2016-11-14 22:01:06 -0800

    more exercises

---
+++ src/jf248/lafore/Ch_15_Proj_2.java
@@ -0,0 +1,229 @@
+package jf248.lafore;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/**
+ * Created by Joshua Freedman on 11/14/2016, based on Lafore Data Structures.
+ */
+public class Ch_15_Proj_2 {
+
+  class DistPar {
+    public int distance;
+    public int parentVert;
+
+    public DistPar(int pv, int d) {
+      distance = d;
+      parentVert = pv;
+    }
+
+  }
+
+  class Vertex {
+    public char label;
+    public boolean isInTree;
+    public LinkedList<Edge> edgeList;
+
+    public Vertex(char lab) {
+      label = lab;
+      isInTree = false;
+      edgeList = new LinkedList<>();
+    }
+
+  }
+
+  class Edge {
+    int weight;
+    Vertex toVertex;
+
+    Edge(int weight, Vertex toVertex) {
+      this.weight = weight;
+      this.toVertex = toVertex;
+    }
+  }
+
+  class Graph {
+    private final int MAX_VERTS = 20;
+    private final int INFINITY = 1000000;
+    private Vertex vertexList[];
+    //    private int adjMat[][];
+    private int nVerts;
+    private int nTree;
+    private DistPar sPath[];
+    private int currentVert;
+    private int startToCurrent;
+
+    public Graph() {
+      vertexList = new Vertex[MAX_VERTS];
+
+//      adjMat = new int[MAX_VERTS][MAX_VERTS];
+      nVerts = 0;
+      nTree = 0;
+//      for (int j = 0; j < MAX_VERTS; j++)
+//        for (int k = 0; k < MAX_VERTS; k++)
+//          adjMat[j][k] = INFINITY;
+      sPath = new DistPar[MAX_VERTS];
+    }
+
+    public void addVertex(char lab) {
+      vertexList[nVerts++] = new Vertex(lab);
+    }
+
+    public void addEdge(int start, int end, int weight) {
+//      adjMat[start][end] = weight;
+      vertexList[start].edgeList.add(new Edge(weight, vertexList[end]));
+    }
+
+    public int getDist(int start, int end) {
+      int w = INFINITY;
+      for (ListIterator<Edge> iter = vertexList[start].edgeList.listIterator(); iter.hasNext(); ) {
+        Edge e = iter.next();
+        if (e.toVertex == vertexList[end]) {
+          w = e.weight;
+        }
+      }
+      return w;
+    }
+
+    public void path(int startTree) {
+
+      vertexList[startTree].isInTree = true;
+      nTree = 1;
+
+      for (int j = 0; j < nVerts; j++) {
+//        int tempDist = adjMat[startTree][j];
+        int tempDist = getDist(startTree, j);
+        sPath[j] = new DistPar(startTree, tempDist);
+      }
+
+
+      while (nTree < nVerts) {
+        int indexMin = getMin();
+        int minDist = sPath[indexMin].distance;
+
+        if (minDist == INFINITY) {
+          System.out.println("There are unreachable vertices");
+          break;
+        } else {
+          currentVert = indexMin;
+          startToCurrent = sPath[indexMin].distance;
+
+
+        }
+
+        vertexList[currentVert].isInTree = true;
+        nTree++;
+        adjust_sPath();
+      }
+
+      displayPaths();
+
+      nTree = 0;
+      for (int j = 0; j < nVerts; j++)
+        vertexList[j].isInTree = false;
+    }
+
+    public int getMin() {
+      int minDist = INFINITY;
+      int indexMin = 0;
+      for (int j = 0; j < nVerts; j++) {
+        if (!vertexList[j].isInTree &&
+            sPath[j].distance < minDist) {
+          minDist = sPath[j].distance;
+          indexMin = j;
+        }
+      }
+      return indexMin;
+    }
+
+    public void adjust_sPath() {
+
+      int column = 0;
+      while (column < nVerts) {
+
+        if (vertexList[column].isInTree) {
+          column++;
+          continue;
+        }
+
+
+//        int currentToFringe = adjMat[currentVert][column];
+        int currentToFringe = getDist(currentVert, column);
+
+        int startToFringe = startToCurrent + currentToFringe;
+
+        int sPathDist = sPath[column].distance;
+
+
+        if (startToFringe < sPathDist) {
+          sPath[column].parentVert = currentVert;
+          sPath[column].distance = startToFringe;
+        }
+        column++;
+      }
+    }
+
+    public void displayPaths() {
+      for (int j = 0; j < nVerts; j++) {
+        System.out.print(vertexList[j].label + "=");
+        if (sPath[j].distance == INFINITY)
+          System.out.print("inf");
+        else
+          System.out.print(sPath[j].distance);
+        char parent = vertexList[sPath[j].parentVert].label;
+        System.out.print("(" + parent + ") ");
+      }
+      System.out.println("");
+    }
+
+    public void displayConnectivity() {
+      System.out.print("  ");
+      for (int i = 0; i < nVerts; i++) {
+        System.out.print(vertexList[i].label + "   ");
+      }
+      System.out.println();
+
+      for (int i = 0; i < nVerts; i++) {
+        System.out.print(vertexList[i].label + " ");
+        for (int j = 0; j < nVerts; j++) {
+          int d = getDist(i, j);
+          if (d == INFINITY) {
+            System.out.print("  ");
+          } else {
+            System.out.print(getDist(i, j) + "");
+          }
+          System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
+    Ch_15_Proj_2 app = new Ch_15_Proj_2();
+    Graph theGraph = app.new Graph();
+    theGraph.addVertex('A');
+    theGraph.addVertex('B');
+    theGraph.addVertex('C');
+    theGraph.addVertex('D');
+    theGraph.addVertex('E');
+
+    theGraph.addEdge(0, 1, 50);
+    theGraph.addEdge(0, 3, 80);
+    theGraph.addEdge(1, 2, 60);
+    theGraph.addEdge(1, 3, 90);
+    theGraph.addEdge(2, 4, 40);
+    theGraph.addEdge(3, 2, 20);
+    theGraph.addEdge(3, 4, 70);
+    theGraph.addEdge(4, 1, 50);
+
+    int vertex = 0;
+    theGraph.displayConnectivity();
+    System.out.println("Shortest paths from vertex " + vertex + ":");
+    theGraph.path(vertex);
+    System.out.println();
+  }
+
+}
+
